38af74fd0646e5867e7881ab6c2bd3576ad508bb
Sergey Ponomarev <stokito@gmail.com>
2013-10-25 14:56:45 +0300

    Bug fix

--- src/groovy/name/stokito/SmartConfigLocaleResolver.groovy
+++ src/groovy/name/stokito/SmartConfigLocaleResolver.groovy
@@ -1,23 +1,36 @@
 package name.stokito
 
 import org.springframework.web.servlet.i18n.SessionLocaleResolver
+import org.springframework.web.util.WebUtils
 
 import javax.servlet.http.HttpServletRequest
 
+/**
+ * Locale resolver that can limit choice user preferred language.
+ * You can define bean of this resolver in resouces.groovy:
+ *
+ *  localeResolver(SmartConfigLocaleResolver) {
+ *      supportedLocales = application.config.supportedLocales ?: []
+ *      defaultLocale = application.config.defaultLocale ?: null
+ *  }
+ *
+ */
 class SmartConfigLocaleResolver extends SessionLocaleResolver {
 
     List<Locale> supportedLocales
 
     @Override
     public Locale resolveLocale(HttpServletRequest request) {
-        Locale localeDesiredByUser = super.resolveLocale(request)
-        localeDesiredByUser
-        // Сначала ищем такую же локаль, если не нашли то локаль с тем же языком, если не нашли то по умолчанию английский
+        Locale localeSavedToSession = getLocaleSavedToSession(request);
+        if (localeSavedToSession) {
+            return localeSavedToSession
+        }
+        Locale localeDesiredByUser = request.locale
         Locale selectedLocale
         if (supportedLocales.contains(localeDesiredByUser)) {
             selectedLocale = localeDesiredByUser
         } else {
-            selectedLocale = findLocaleWithSameLanguage(localeDesiredByUser, supportedLocales)
+            selectedLocale = findLocaleWithSameLanguage(localeDesiredByUser)
         }
         if (!selectedLocale) {
             selectedLocale = determineDefaultLocale(request);
@@ -26,7 +39,12 @@ class SmartConfigLocaleResolver extends SessionLocaleResolver {
 
     }
 
-    private static Locale findLocaleWithSameLanguage(Locale localeDesiredByUser, List<Locale> supportedLocales) {
+    private static Locale getLocaleSavedToSession(HttpServletRequest request) {
+        Locale localeSavedToSession = (Locale) WebUtils.getSessionAttribute(request, LOCALE_SESSION_ATTRIBUTE_NAME)
+        return localeSavedToSession
+    }
+
+    private Locale findLocaleWithSameLanguage(Locale localeDesiredByUser) {
         Locale localeWithSameLanguage = supportedLocales.find({ it.language == localeDesiredByUser.language })
         return localeWithSameLanguage
     }
